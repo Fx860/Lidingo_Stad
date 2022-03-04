8ce40295ae2a1664dbcff5658b14eed84a7abaf2
Fredrik Gustafsson <gustafsson860@live.com>
2022-03-04 10:10:30 +0100

    fixat till undefined- cod på org nr & faknr

--- app.js
+++ app.js
@@ -1,45 +1,54 @@
 // api url
-const api_url = "https://raw.githubusercontent.com/HackersOfSweden/Ekonomy/main/Levfakturor_Lidingo_Stad_2018.json";
+const api_url =
+  "https://raw.githubusercontent.com/HackersOfSweden/Ekonomy/main/Levfakturor_Lidingo_Stad_2018.json";
+const SEARCHAPI = newFunction();
+function newFunction() {
+  return "https://raw.githubusercontent.com/HackersOfSweden/Ekonomy/main/Levfakturor_Lidingo_Stad_2018.json";
+}
+
 // Defining async function
 async function getapi(url) {
-    
-    // Storing response
-    const response = await fetch(url);
-    
-    // Storing data in form of JSON
-    var data = await response.json();
-    console.log(data);
-    if (response) {
-        hideloader();
-    }
-   show(data);
+  // Storing response
+  const response = await fetch(url);
+
+  // Storing data in form of JSON
+  var data = await response.json();
+  console.log(data);
+  if (response) {
+    hideloader();
+  }
+  show(data);
 }
 // Calling that async function
 getapi(api_url);
+async function funcName(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+}
 // Function to hide the loader
 function hideloader() {
-    document.getElementById('loading').style.display = 'none';
+  document.getElementById("loading").style.display = "none";
+  const search = document.getElementById("search");
 }
 // Function to define innerHTML for HTML table
 function show(data) {
-  let tab = 
-      `<tr>
-      <th>Fakt.nr</th>
+  let tab = `<tr>
+      <th>Faktnr</th>
       <th>Förvaltning</th>
-      <th>konterat</th>
-      <th>konto</th>
+      <th>Konterat</th>
+      <th>Konto</th>
       <th>Leverantör</th>
-      <th>org.nr</th>
+      <th>Orgnr</th>
       </tr>`;
-  // Loop to access all rows 
+  // Loop to access all rows
   for (let r of data) {
-      tab += `<tr> 
-  <td>${r.faktnr} </td>
+    tab += `<tr> 
+  <td>${r["fakt.nr"]} </td>
   <td>${r.forvaltning}</td>
   <td>${r.konterat}</td> 
   <td>${r.konto}</td> 
   <td>${r.leverantor}</td>
-  <td>${r.orgnr}</td>                 
+  <td>${r["org.nr"]}</td>                 
   </tr>`;
   }
   // Setting innerHTML as tab variable
